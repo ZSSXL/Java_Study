e3ae0075d5a281c3224c655ae9023cd11bdf1533
ZSSXL <1271130458@qq.com>
2020-08-14 10:49:26 +0800

    anything

--- lock_demo/src/main/java/com/zss/lock/distributed/RedisLockV1.java
+++ lock_demo/src/main/java/com/zss/lock/distributed/RedisLockV1.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -28,25 +29,23 @@ public class RedisLockV1 {
         this.redisUtil = redisUtil;
     }
 
-    private final int flag = 3;
-
     /**
      * 分布式锁第一版本
+     * 每隔十秒执行一次
      */
+    @Scheduled(cron = "0/10 * * * * ?")
     public void redisLockV1() {
-        for (int i = 0; i < flag; i++) {
-            // 调用setNx
-            Boolean result = redisUtil.setNx(Const.KEY, Const.VALUE);
-            // 判断是否成功
-            if (result != null && result) {
-                // 执行业务逻辑
-                FunctionUtil.runBusiness();
-                // 释放锁
-                releaseLock();
-            } else {
-                // 不成功则获取锁失败
-                log.warn("没有获得分布式锁：[{}]", Const.KEY);
-            }
+        // 调用setNx
+        Boolean result = redisUtil.setNx(Const.KEY, Const.VALUE);
+        // 判断是否成功
+        if (result != null && result) {
+            // 执行业务逻辑
+            FunctionUtil.runBusiness();
+            // 释放锁
+            releaseLock();
+        } else {
+            // 不成功则获取锁失败
+            log.warn("没有获得分布式锁：[{}]", Const.KEY);
         }
     }
 
@@ -54,27 +53,18 @@ public class RedisLockV1 {
      * Redis分布式锁 - 将过期释放锁的权限交给Redis
      */
     public void redisLockV2() {
-        for (int i = 0; i < flag; i ++){
-            // 调用SetNx
-            Boolean result = redisUtil.setNx(Const.KEY, Const.VALUE);
-            if (result != null && result) {
-                // 给锁添加有效时间 ： 当前时间 + 有效期
-                Boolean expire = redisUtil.expire(Const.KEY, Const.EXPIRE_TIME);
-                if (expire != null && expire) {
-                    // 执行业务逻辑
-                    FunctionUtil.runBusiness();
-                    // 释放锁
-                    releaseLock();
-                } else {
-                    // 释放锁
-                    releaseLock();
-                    // 不成功则获取锁失败
-                    log.warn("没有获得分布式锁：[{}]", Const.KEY);
-                }
-            } else {
-                // 不成功则获取锁失败
-                log.warn("没有获得分布式锁：[{}]", Const.KEY);
-            }
+        // 调用SetNx
+        Boolean result = redisUtil.setNx(Const.KEY, Const.VALUE);
+        if (result != null && result) {
+            // 给锁添加有效时间 ： 当前时间 + 有效期
+            redisUtil.expire(Const.KEY, Const.EXPIRE_TIME);
+            // 执行业务逻辑
+            FunctionUtil.runBusiness();
+            // 释放锁
+            releaseLock();
+        } else {
+            // 不成功则获取锁失败
+            log.warn("没有获得分布式锁：[{}]", Const.KEY);
         }
     }
 
@@ -83,17 +73,15 @@ public class RedisLockV1 {
      * 调用setNxEx方法
      */
     public void redisLockV2Plus() {
-        for (int i = 0; i < flag; i ++){
-            String result = redisUtil.setNxEx(Const.KEY, Const.VALUE, Const.EXPIRE_TIME);
-            if (result != null && StringUtils.isEmpty(result)) {
-                // 执行业务逻辑
-                FunctionUtil.runBusiness();
-                // 释放锁
-                releaseLock();
-            } else {
-                // 不成功则获取锁失败
-                log.warn("没有获得分布式锁：[{}]", Const.KEY);
-            }
+        String result = redisUtil.setNxEx(Const.KEY, Const.VALUE, Const.EXPIRE_TIME);
+        if (result != null && StringUtils.isEmpty(result)) {
+            // 执行业务逻辑
+            FunctionUtil.runBusiness();
+            // 释放锁
+            releaseLock();
+        } else {
+            // 不成功则获取锁失败
+            log.warn("没有获得分布式锁：[{}]", Const.KEY);
         }
     }
 
@@ -101,26 +89,24 @@ public class RedisLockV1 {
      * redis分布式锁 - 将释放过期锁的权限交给其他服务器
      */
     public void redisLockV3() {
-        for (int i = 0; i < flag; i ++){
-            Boolean result = redisUtil.setNx(Const.KEY,
-                    String.valueOf(TimeUtil.getCurrentTimestamp() + Const.EXPIRE_TIME));
-            if (result != null && result) {
-                // 获取到锁，执行业务逻辑
+        Boolean result = redisUtil.setNx(Const.KEY,
+                String.valueOf(TimeUtil.getCurrentTimestamp() + Const.EXPIRE_TIME));
+        if (result != null && result) {
+            // 获取到锁，执行业务逻辑
+            FunctionUtil.runBusiness();
+            // 释放锁
+            releaseLock();
+        } else {
+            // 未获取到锁，判断有效期是否过期
+            String timeout = redisUtil.get(Const.KEY);
+            // 如果timeout不为空且获取到的timeout小于当前的时间戳
+            if (timeout != null && Long.parseLong(timeout) < TimeUtil.getCurrentTimestamp() + Const.EXPIRE_TIME) {
+                // 执行业务逻辑
                 FunctionUtil.runBusiness();
                 // 释放锁
                 releaseLock();
             } else {
-                // 未获取到锁，判断有效期是否过期
-                String timeout = redisUtil.get(Const.KEY);
-                // 如果timeout不为空且获取到的timeout小于当前的时间戳
-                if (timeout != null && Long.parseLong(timeout) < TimeUtil.getCurrentTimestamp() + Const.EXPIRE_TIME) {
-                    // 执行业务逻辑
-                    FunctionUtil.runBusiness();
-                    // 释放锁
-                    releaseLock();
-                } else {
-                    log.warn("没有获得分布式锁：[{}]", Const.KEY);
-                }
+                log.warn("没有获得分布式锁：[{}]", Const.KEY);
             }
         }
     }
@@ -130,34 +116,32 @@ public class RedisLockV1 {
      * 使用getSet方法
      */
     public void redisLockV3Plus() {
-        for (int i = 0; i < flag; i ++){
-            Boolean result = redisUtil.setNx(Const.KEY,
-                    String.valueOf(TimeUtil.getCurrentTimestamp() + Const.EXPIRE_TIME));
-            if (result != null && result) {
-                // 获取到锁，执行业务逻辑
-                FunctionUtil.runBusiness();
-                // 释放锁
-                releaseLock();
-            } else {
-                // 未获取到锁，判断有效期是否过期
-                String timeout = redisUtil.get(Const.KEY);
-                // 如果timeout不为空且获取到的timeout小于当前的时间戳
-                if (timeout != null && Long.parseLong(timeout) < TimeUtil.getCurrentTimestamp() + Const.EXPIRE_TIME) {
-                    String getSetValue = redisUtil.getSet(Const.KEY,
-                            String.valueOf(TimeUtil.getCurrentTimestamp() + Const.EXPIRE_TIME));
-                    // 如果getSetValue为空 -> 说明锁已经被释放了，可以获得锁
-                    // 或者如果getSetValue等于之前获得的timeout -> 说明在这之间没有其他服务器去改变这个value，也可以获得锁
-                    if (StringUtils.isEmpty(getSetValue) || StringUtils.equals(timeout, getSetValue)) {
-                        // 执行业务逻辑
-                        FunctionUtil.runBusiness();
-                        // 释放锁
-                        releaseLock();
-                    } else {
-                        log.warn("没有获得分布式锁：[{}]", Const.KEY);
-                    }
+        Boolean result = redisUtil.setNx(Const.KEY,
+                String.valueOf(TimeUtil.getCurrentTimestamp() + Const.EXPIRE_TIME));
+        if (result != null && result) {
+            // 获取到锁，执行业务逻辑
+            FunctionUtil.runBusiness();
+            // 释放锁
+            releaseLock();
+        } else {
+            // 未获取到锁，判断有效期是否过期
+            String timeout = redisUtil.get(Const.KEY);
+            // 如果timeout不为空且获取到的timeout小于当前的时间戳
+            if (timeout != null && Long.parseLong(timeout) < TimeUtil.getCurrentTimestamp() + Const.EXPIRE_TIME) {
+                String getSetValue = redisUtil.getSet(Const.KEY,
+                        String.valueOf(TimeUtil.getCurrentTimestamp() + Const.EXPIRE_TIME));
+                // 如果getSetValue为空 -> 说明锁已经被释放了，可以获得锁
+                // 或者如果getSetValue等于之前获得的timeout -> 说明在这之间没有其他服务器去改变这个value，也可以获得锁
+                if (StringUtils.isEmpty(getSetValue) || StringUtils.equals(timeout, getSetValue)) {
+                    // 执行业务逻辑
+                    FunctionUtil.runBusiness();
+                    // 释放锁
+                    releaseLock();
                 } else {
                     log.warn("没有获得分布式锁：[{}]", Const.KEY);
                 }
+            } else {
+                log.warn("没有获得分布式锁：[{}]", Const.KEY);
             }
         }
     }
